4c480fe10e7d7911beb8536e6d356eda2bf18deb
Bortokox <bortokox@gmail.com>
2020-09-10 11:48:54 +0200

    Initial Commit

---
+++ DatinApp.Api/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatinApp.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatinApp.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
