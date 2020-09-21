128335d41496cb3a1567cc09a9c638caec9cbfd6
Bortokox <bortokox@gmail.com>
2020-09-21 14:18:21 +0200

    End of Section 8

---
+++ DatinApp.Api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using AutoMapper;
+using DatinApp.Api.Dtos;
+using DatinApp.Api.Models;
+
+namespace DatinApp.Api.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
+                    src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
+                    src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+    }
+}
